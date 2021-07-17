0a974ca5d0a2742d63ab70b2453c9b7c267a4fc9
chen edri <chenedri22@gmail.com>
2021-07-17 12:16:15 +0300

    Updates

---
+++ precache-manifest.a8874dd00b88d01f14d364560cfb3312.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64deff4c0f6ab7f2e8c9bb6b3a51d08",
+    "url": "/layline-front/index.html"
+  },
+  {
+    "revision": "b7a31f90c06ce430cfd3",
+    "url": "/layline-front/static/css/main.4e551817.chunk.css"
+  },
+  {
+    "revision": "cfa394a1896555b8a0fa",
+    "url": "/layline-front/static/js/2.d8e928f0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/layline-front/static/js/2.d8e928f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a31f90c06ce430cfd3",
+    "url": "/layline-front/static/js/main.684d6528.chunk.js"
+  },
+  {
+    "revision": "365db20987219a0f7865",
+    "url": "/layline-front/static/js/runtime-main.e0e27964.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/layline-front/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/layline-front/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/layline-front/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/layline-front/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/layline-front/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/layline-front/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/layline-front/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/layline-front/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/layline-front/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/layline-front/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  }
+]);
